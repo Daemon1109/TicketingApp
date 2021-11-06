33dcd3b160e2c34cf3db979f754ce27275a86400
Daemon1109 <sachdevsahil65@gmail.com>
2021-11-06 13:01:29 +0000

    Adds user signin route functionality

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { BadRequestError } from '../errors/bad-request-error';
+import jwt from 'jsonwebtoken';
 
 import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/User';
+import { Password } from '../services/password';
 
 const router = express.Router();
 
@@ -12,8 +16,44 @@ router.post(
     body('password').trim().notEmpty().withMessage('Please provide a password'),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.send('Hi from signin.ts');
+  async (req: Request, res: Response) => {
+    // Extract information from request body
+    const { email, password } = req.body;
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+
+    // If no, throw a BadRequestError
+    if (!existingUser) {
+      throw new BadRequestError('Invalid Credentials');
+    }
+
+    // Compare stored and provided passwords
+    const passwordMatches = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    // If passwords don't match, throw a BadRequestError
+    if (!passwordMatches) {
+      throw new BadRequestError('Invalid Credentials');
+    }
+
+    // Generate a JWT for user
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store the JWT in session object
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
   }
 );
 
