567523c16d89e79a616a10682b50076257ee1bae
Daemon1109 <sachdevsahil65@gmail.com>
2021-11-08 20:21:34 +0000

    Modifies _app to use cutom app

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,9 +1,42 @@
 import '../styles/globals.css'
+import App, { AppInitialProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.css'         // Add bootstrap support
-import type { AppProps } from 'next/app'
+import type { AppContext, AppProps } from 'next/app'
+import buildAxiosClient from './api/build-axios-client'
+import { CurrentUser } from '../modals/currentuser';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface Props {
+  currentUser?: CurrentUser
+}
+
+// TODO change the AppProps interface to take current user as an input
+function MyApp({ Component, pageProps, currentUser }: AppProps & Props) {
+  return (
+    <div>
+      <h1>Header! {currentUser?.email}</h1>
+      <Component {...pageProps} />
+    </div>
+  )
+}
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  const axiosClient = buildAxiosClient(appContext.ctx);
+
+  const response = await axiosClient.get('/api/users/currentuser');
+  const currentUser = (response.data.currentUser) ? (
+    response.data.currentUser
+  ) : undefined;
+
+  const appProps: AppInitialProps = await App.getInitialProps(appContext);   // Using Next provided App to check & call getInitialProps methods of pages
+  
+  // const pageProps = (appContext.Component.getInitialProps) ? (
+  //   await appContext.Component.getInitialProps(appContext.ctx)
+  // ) : (
+  //   {}
+  // );                                              // Manually checking & calling if a page have getInitialProps
+      
+  return {...appProps, currentUser};
+  // return {pageProps, currentUser};
 }
 
 export default MyApp
